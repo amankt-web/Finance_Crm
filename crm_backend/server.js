0cf63432cb4821e487d21458c866bd48e0bb5f8f
Aman <amankr88640@gmail.com>
2024-11-04 18:48:18 +0530

    done

--- crm_backend/server.js
+++ crm_backend/server.js
@@ -44,10 +44,11 @@ app.use('/api',graphRoutes);
 
 app.use(express.static(path.join(__dirname, 'dist/client_crm/browser')));
 
-// Route to serve the server index.html file
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/client_crm/server/index.server.html'));
+// Route to serve the Angular app's index.html for all non-API routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/client_crm/browser/index.html'));
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
